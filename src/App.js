c6adcb687c068832b5b9f274aa619d86432c5212
stefano <gavioli.stefano.9@gmail.com>
2020-09-28 21:28:45 +0200

    First work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import MainPage from "./containers/MainPage";
 import Planimetria from "./containers/Planimetria";
-import {Route, Router, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import PianoCovid from "./containers/PianoCovid";
 import Camere from "./containers/Camere";
 import NotFound from "./containers/NotFound";
